191816b1dfa1e83c55062c81d5b4a42ed9c6265e
ye4241 <6803102+ye4241@users.noreply.github.com>
2022-12-27 10:00:07 +0800

    refactor nightcafe with signin;

--- src/nightcafe.py
+++ src/nightcafe.py
@@ -1,20 +1,34 @@
 """
 new Env('nightcafe');
-nightcafe daily credits
 0 22 * * * nightcafe.py
 """
 import os
+
 import requests
 import urllib3
 
 urllib3.disable_warnings()
 
 if __name__ == '__main__':
-    url = "https://us-central1-nightcafe-creator.cloudfunctions.net/api/credits/topup"
-    headers = {
-        "accept": "application/json, text/plain, */*",
-        "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-        "x-auth-token": os.environ.get("NIGHTCAFE_TOKEN")
-    }
-    response = requests.request("POST", url, headers=headers, verify=False)
-    print(response.json())
+    users = [token.split(';') for token in os.environ.get('NIGHTCAFE_TOKEN').split('&')]
+
+    session = requests.Session()
+    session.verify = False
+    session.headers.update({
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54',
+        'referer': 'https://creator.nightcafe.studio/',
+    })
+
+    for user in users:
+        print(f'login user: {user[0]}')
+        login = session.post(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD_bN4JwaaUIuYIOZ2cTvHrh0LRUYTXnfI',
+            json={'returnSecureToken': True, 'email': user[0], 'password': user[1]}
+        ).json()
+        print(login)
+        print('get daily credits')
+        topup = session.post(
+            'https://us-central1-nightcafe-creator.cloudfunctions.net/api/credits/topup',
+            headers={'x-auth-token': login['idToken']}
+        ).json()
+        print(topup)
