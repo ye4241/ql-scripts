62d118a0e8be7b70a5c5b6633e5b6e02ee7b6476
ye4241 <6803102+ye4241@users.noreply.github.com>
2023-03-21 19:59:54 +0800

    add more logic for nightcafe;

--- src/nightcafe.py
+++ src/nightcafe.py
@@ -3,6 +3,9 @@ new Env('nightcafe');
 5 8 * * * nightcafe.py
 '''
 import os
+import random
+import time
+
 import requests
 import urllib3
 
@@ -11,7 +14,7 @@ urllib3.disable_warnings()
 ENV_KEY = 'NIGHTCAFE_TOKEN'
 
 
-def get_daily_credits(email, password):
+def login(email, password):
     session = requests.Session()
     session.verify = False
     session.headers.update({
@@ -19,20 +22,66 @@ def get_daily_credits(email, password):
         'referer': 'https://creator.nightcafe.studio/',
     })
     print(f'login user: {email}')
-    login = session.post(
+    user = session.post(
         'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD_bN4JwaaUIuYIOZ2cTvHrh0LRUYTXnfI',
         json={'returnSecureToken': True, 'email': email, 'password': password}
     ).json()
-    print(login)
-    print('get daily credits')
-    topup = session.post(
-        'https://us-central1-nightcafe-creator.cloudfunctions.net/api/credits/topup',
-        headers={'x-auth-token': login['idToken']}
+    session.headers.update({
+        'x-auth-token': user['idToken'],
+    })
+    print(user)
+    return (session, user['localId'])
+
+
+def get_connect_credits(session: requests.Session, user_id: str):
+    print('get connect credits')
+    result = session.post(
+        f'https://us-central1-nightcafe-creator.cloudfunctions.net/api/inboxMessage/user-{user_id}:message-userReceivedBadge-connectAnonymous:subject-connectAnonymous/action',
     ).json()
-    print(topup)
+    print(result)
+
+
+def get_daily_credits(session: requests.Session):
+    print('get daily credits')
+    result = session.post(
+        'https://us-central1-nightcafe-creator.cloudfunctions.net/api/credits/topup',
+    ).json()
+    print(result)
+
+
+def get_vote_daily_credits(session: requests.Session, user_id: str):
+    print('get vote daily credits')
+    count = 20
+    game = session.get(
+        'https://creator.nightcafe.studio/_next/data/1GRtkxQ0IkSk9uI4hvJm9/game/daily-challenge-vote.json?gameId=daily-challenge-vote').json()
+    game_id = game['pageProps']['__N_REDIRECT'].rstrip('?').split('/')[2]
+    print('game_id', game_id)
+    game_page = session.get(
+        f'https://us-central1-nightcafe-creator.cloudfunctions.net/api/challenge/{game_id}/entries/voting?page=1').json()
+    entries = game_page['entries']
+    for entry in entries[:count]:
+        entry_id = entry['id']
+        rating = random.randint(1, 5)
+        print('vote', entry_id, rating)
+        vote = session.post(
+            f'https://us-central1-nightcafe-creator.cloudfunctions.net/api/challenge/{game_id}/entry/{entry_id}/vote',
+            json={
+                'voteKey': entry['voteKey'],
+                'rating': rating
+            }).json()
+        print(vote['status'])
+    time.sleep(1)
+    claim = session.post(
+        f'https://us-central1-nightcafe-creator.cloudfunctions.net/api/inboxMessage/user-{user_id}:message-userReceivedBadge-dailyChallengeVoter:subject-dailyChallengeVoter:unique-1/action',
+        json={}
+    ).json()
+    print(claim)
 
 
 if __name__ == '__main__':
     users = [token.split(';') for token in os.environ.get(ENV_KEY).split('&')]
     for user in users:
-        get_daily_credits(user[0], user[1])
+        (session, user_id) = login(user[0], user[1])
+        get_daily_credits(session)
+        get_connect_credits(session, user_id)
+        get_vote_daily_credits(session, user_id)
