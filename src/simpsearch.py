412f2e128b7053f78ad49706ba64f4b5e66268bd
ye4241 <6803102+ye4241@users.noreply.github.com>
2023-03-22 13:35:32 +0800

    fix simpsearch checkin bug;

--- src/simpsearch.py
+++ src/simpsearch.py
@@ -11,26 +11,32 @@ urllib3.disable_warnings()
 ENV_KEY = 'SIMPSEARCH_COOKIE'
 
 
-def get_current_user(cookie):
-    print('获取用户')
-    response = requests.get('https://www.simpsearch.com/wp-admin/admin-ajax.php?action=get_current_user', headers={
+def login(cookie):
+    session = requests.Session()
+    session.headers.update({
         'cookie': cookie,
-    }, verify=False).json()
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.69'})
+    session.verify = False
+    return session
+
+
+def get_current_user(session):
+    print('获取用户')
+    response = session.get('https://www.simpsearch.com/wp-admin/admin-ajax.php?action=get_current_user').json()
     print(response)
 
 
-def user_checkin(cookie):
+def user_checkin(session):
     print('开始签到')
-    response = requests.post('https://www.simpsearch.com/wp-admin/admin-ajax.php', data={
-        'action': 'user_checkin'
-    }, headers={
-        'cookie': cookie,
-    }, verify=False).json()
+    response = session.post('https://www.simpsearch.com/wp-admin/admin-ajax.php', headers={
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    }, data='action=user_checkin').json()
     print(response)
 
 
 if __name__ == '__main__':
     users = [token for token in os.environ.get(ENV_KEY).split('&')]
     for user in users:
-        get_current_user(user)
-        user_checkin(user)
+        session = login(user)
+        get_current_user(session)
+        user_checkin(session)
